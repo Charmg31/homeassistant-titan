425a54cf5578f86953745752a2b0395b583dbeb3
Charmg31 <147717717+Charmg31@users.noreply.github.com>
2025-10-28 07:54:22 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,2 @@
-def get_device_gen(device_model: str) -> int:
-    """Retourne la génération de l'appareil selon son modèle."""
 
-    titan_gen1_models = ["Titan 2400", "Titan 3200", "Titan PRO"]  # Modèles Gen 1 Titan (exemples)
-    if device_model in titan_gen1_models:
-        return 1
-    else:
-        return 2
 
