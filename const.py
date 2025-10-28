d66c2ac32c3bebce5faeb3654099c1063003d380
Charmg31 <147717717+Charmg31@users.noreply.github.com>
2025-10-28 07:52:14 +0100

    Update const.py

--- const.py
+++ const.py
@@ -1,15 +1,1 @@
-from homeassistant.const import Platform
 
-DOMAIN = "titan_izypower"  # Met le même nom que ton dossier d'intégration
-DEFAULT_PORT = 8899        # Mets le port par défaut de la Titan si besoin
-DEFAULT_SCAN_INTERVAL = 30
-PLATFORMS = [
-    Platform.SENSOR
-]
-
-SUPPORTED_MODELS = [
-    "Titan 2400",
-    "Titan 3200",
-    "Titan PRO",
-    # Ajoute ici tous les modèles Titan IzyPower à supporter
-]
