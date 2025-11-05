43773db3ea8ab11d4b9de6025fad0bb578ebf81d
Charmg31 <147717717+Charmg31@users.noreply.github.com>
2025-11-05 10:03:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 """Izypower Titan battery integration."""
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
+from homeassistant.config_entries import ConfigEntry
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Izypower Titan integration."""
-    # Initial setup if necessary
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Initial setup if needed."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up integration from config flow entry."""
+    hass.data.setdefault("izypower_titan", {})
+
+    # Load platforms (sensor, switch, etc.)
+    hass.config_entries.async_setup_platforms(entry, ["sensor", "switch"])
     return True
